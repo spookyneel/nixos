947c775270b9c80b2a9be8b83dc55eaf5af90823
Neel Verma <spookyneel@gmail.com>
2026-06-06 20:08:57 +0530

    basic DMS setup w/ hyprland binds, fonts, and better font rendering settings

--- configuration.nix
+++ configuration.nix
@@ -70,7 +70,21 @@
   programs.firefox.enable = true;
 
   # DMS
-  programs.dms-shell.enable = true;
+  programs.dms-shell = {
+  enable = true;
+
+  systemd = {
+    enable = true;             # Systemd service for auto-start
+    restartIfChanged = true;   # Auto-restart dms.service when dms-shell changes
+  };
+  
+  # Core features
+  enableSystemMonitoring = true;     # System monitoring widgets (dgop)
+  enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
+  enableAudioWavelength = true;      # Audio visualizer (cava)
+  enableCalendarEvents = true;       # Calendar integration (khal)
+  enableClipboardPaste = true;       # Pasting from the clipboard history (wtype)
+  }; 
 
   programs.hyprland = {
     enable = true;
@@ -82,12 +96,38 @@
    enable = true;
    defaultEditor = true;
   };
+ 
+  services.displayManager.sddm = {
+    enable = true;
+    # Enables experimental Wayland support
+    wayland.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     vim     
     wget
     wezterm
     git
+    kitty
+  ];
+
+  fonts.fontconfig = {
+    enable = true;
+    hinting = {
+      enable = true;
+      style = "slight";
+    };
+    subpixel.rgba = "none";
+  };
+
+  fonts.packages = with pkgs; [
+    inter
+    geist-font
+    jetbrains-mono
+    iosevka
+    noto-fonts
+    noto-fonts-color-emoji
+    nerd-fonts.jetbrains-mono
   ];
 
   services.openssh.enable = true;
